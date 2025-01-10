99144d528c992b7e9ee07a8287642b501c432856
Username <87919916+AP0tato@users.noreply.github.com>
2025-01-10 09:20:56 -0500

    First Commit

---
+++ FileHelper.java
@@ -0,0 +1,7 @@
+public class FileHelper 
+{
+    private static String FILE = "default.txt";
+
+    public static String getFile() { return FILE; }
+    public static void setFile(String file) { FILE = file; }
+}
