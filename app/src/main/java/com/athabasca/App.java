a731d144ab159ccc438dc8e0dcd9d19a91d983c1
Username <87919916+AP0tato@users.noreply.github.com>
2025-01-13 12:43:58 -0500

    Service Key

--- app/src/main/java/com/athabasca/App.java
+++ app/src/main/java/com/athabasca/App.java
@@ -1,8 +1,23 @@
 package com.athabasca;
 
+import java.io.FileInputStream;
+
 import javax.swing.JFrame;
 
 public class App extends JFrame
 {
     public String getGreeting() { return "Hi"; }
+
+    public App()
+    {
+        FileInputStream serviceAccount =
+            new FileInputStream("/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setDatabaseUrl("https://finalproject12-1fd07-default-rtdb.firebaseio.com")
+            .build();
+
+        FirebaseApp.initializeApp(options);
+    }
 }
