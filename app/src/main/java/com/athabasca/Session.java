00e949200912f38c10b3021684879e7914d25ec1
Username <87919916+AP0tato@users.noreply.github.com>
2025-01-14 12:13:18 -0500

    getters and setters

--- app/src/main/java/com/athabasca/Session.java
+++ app/src/main/java/com/athabasca/Session.java
@@ -8,7 +8,7 @@ public class Session
      * 0 = lowest, employees
      * 1 = admins
      */
-    private static int PERMISSION = -1;
+    private int PERMISSION = -1;
     private String f_name;
     private String l_name;
     private String email;
@@ -19,4 +19,82 @@ public class Session
     {
         
     }
+
+    /**
+     * @return int return the PERMISSION
+     */
+    public int getPermissin() {
+        return PERMISSION;
+    }
+
+    /**
+     * @return String return the f_name
+     */
+    public String getF_name() {
+        return f_name;
+    }
+
+    /**
+     * @param f_name the f_name to set
+     */
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    /**
+     * @return String return the l_name
+     */
+    public String getL_name() {
+        return l_name;
+    }
+
+    /**
+     * @param l_name the l_name to set
+     */
+    public void setL_name(String l_name) {
+        this.l_name = l_name;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the employee_id
+     */
+    public String getEmployee_id() {
+        return employee_id;
+    }
+
+    /**
+     * @param employee_id the employee_id to set
+     */
+    public void setEmployee_id(String employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    /**
+     * @return List<String> return the assigned
+     */
+    public List<String> getAssigned() {
+        return assigned;
+    }
+
+    /**
+     * @param assigned the assigned to set
+     */
+    public void setAssigned(List<String> assigned) {
+        this.assigned = assigned;
+    }
+
 }
