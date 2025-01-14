814fdc54ccfa1e98fc6b81049edd52eb07d16e3f
Username <87919916+AP0tato@users.noreply.github.com>
2025-01-13 21:56:01 -0500

    Things

--- app/src/test/java/com/athabasca/AppTest.java
+++ app/src/test/java/com/athabasca/AppTest.java
@@ -12,7 +12,6 @@ import javax.swing.SwingUtilities;
 import org.junit.Test;
 
 public class AppTest {
-<<<<<<< Updated upstream
     @Test public void runApp() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
         SwingUtilities.invokeLater(() -> {
@@ -27,10 +26,5 @@ public class AppTest {
             }
         });
         latch.await();
-=======
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        //assertNotNull("app should have a greeting", classUnderTest.getGreeting());
->>>>>>> Stashed changes
     }
 }
