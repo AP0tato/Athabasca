15bced3a9f35ad25525e82e9259cd81c7f2c3abc
Username <87919916+AP0tato@users.noreply.github.com>
2025-01-15 11:50:58 -0500

    heapsort works

--- app/src/main/java/com/athabasca/Heapsort.java
+++ app/src/main/java/com/athabasca/Heapsort.java
@@ -1,6 +1,7 @@
 package com.athabasca;
 
 import java.util.List;
+import java.util.Map;
 
 public class Heapsort 
 {
@@ -12,7 +13,7 @@ public class Heapsort
      * @param list The list of strings to sort.
      * @return The sorted list.
      */
-    public static List<String> sort(List<String> list)
+    public static List<Map.Entry<String, Integer>> sort(List<Map.Entry<String, Integer>> list)
     {
         int n = list.size();
 
@@ -24,7 +25,7 @@ public class Heapsort
         for(int i = n - 1; i >= 0; i--) // Loop n times where n is the size of the list -> O(n)
         {
             // Swap the root (maximum element) with the last element in the heap
-            String swap = list.get(0);
+            Map.Entry<String, Integer> swap = list.get(0);
             list.set(0, list.get(i));
             list.set(i, swap);
 
@@ -42,24 +43,24 @@ public class Heapsort
      * @param index The root index of the subtree.
      * @param size The size of the heap.
      */
-    private static void heapify(List<String> list, int index, int size)
+    private static void heapify(List<Map.Entry<String, Integer>> list, int index, int size)
     {
         int largest = index; // Initialize the largest element as the root
         int left = 2 * index + 1; // Left index
         int right = 2 * index + 2; // Right index
 
         // Check if the left index is larger than the root
-        if(left < size && list.get(left).compareTo(list.get(largest)) > 0)
+        if(left < size && list.get(left).getKey().compareTo(list.get(largest).getKey()) > 0)
             largest = left;
         
         // Check if the right index is larger than the current largest
-        if(right < size && list.get(right).compareTo(list.get(largest)) > 0)
+        if(right < size && list.get(right).getKey().compareTo(list.get(largest).getKey()) > 0)
             largest = right;
             
         // If the largest is not the root, swap and continue heapifying
         if(largest != index) 
         {
-            String swap = list.get(largest);
+            Map.Entry<String, Integer> swap = list.get(largest);
             list.set(largest, list.get(index));
             list.set(index, swap);
 
