9ea2bd79c85824c0c017f3f8dcca37fc2733b276
PriSey <80794757+PriSey@users.noreply.github.com>
2025-01-18 00:11:35 -0500

    s

--- app/src/main/java/com/athabasca/ButtonRenderer.java
+++ app/src/main/java/com/athabasca/ButtonRenderer.java
@@ -4,14 +4,15 @@ import javax.swing.JButton;
 import javax.swing.table.TableCellRenderer;
 import java.awt.Component;
 import javax.swing.JTable;
+
 class ButtonRenderer extends JButton implements TableCellRenderer {
     public ButtonRenderer() {
         setOpaque(true);
     }
 
     @Override
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                                                   boolean hasFocus, int row, int column) {
+    public Component getTableCellRendererComponent(JTable table, Object value,
+                                                   boolean isSelected, boolean hasFocus, int row, int column) {
         setText((value == null) ? "" : value.toString());
         return this;
     }
